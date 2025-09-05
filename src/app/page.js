6e85942c5fe0ba9e9bfc31e749f9ad9ddb2fd3f7
Milad Rastin <miladrastin@Milads-MacBook-Air.local>
2025-09-05 19:07:44 +0330

    feat(home-page) : dynamic lastest products

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,20 @@ import Banner from "@/components/templates/index/banner/Banner";
 import Latest from "@/components/templates/index/latest/Latest";
 import Promote from "@/components/templates/index/promote/Promote";
 
+//
+import connectToDB from "@/configs/db";
+import productModel from "@/models/Product";
 
 export default async function Home() {
 
+  await connectToDB();
+
+  const products = await productModel.find({})
 
   return (
     <>
       <Banner />
-      <Latest />
+      <Latest products={JSON.parse(JSON.stringify(products))} />
       <Promote />
       <Articles />
     </>
