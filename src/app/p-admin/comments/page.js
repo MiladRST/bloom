bb56115de6c8068e11d91813f7e24d7b5e11960f
Milad Rastin <miladrastin@Milads-MacBook-Air.local>
2025-09-13 17:52:14 +0330

    feat(p-admin) : users, comments

--- src/app/p-admin/comments/page.js
+++ src/app/p-admin/comments/page.js
@@ -1,7 +1,16 @@
-
+import connectToDB from "@/configs/db"
+import commentModel from "@/models/Comment"
 
 export default async function() {
+        await connectToDB()
+    
+        const comments = await commentModel.find({}).populate("productID").populate("user")
+
+        console.log(comments);
+        
     return(
-        <h1>p-admin/comments</h1>
+        <>
+            <h1>p-admin/comments</h1>
+        </>
     )
 }
--- src/app/p-admin/users/page.js
+++ src/app/p-admin/users/page.js
@@ -1,7 +1,42 @@
-
+import connectToDB from "@/configs/db"
+import userModel from "@/models/User"
 
 export default async function() {
+    await connectToDB()
+
+    const users = await userModel.find({} , "name email phone role")
+    
     return(
-        <h1>p-admin/users</h1>
+        <>
+            <h1>p-admin/users</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Role</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map( (user, index) => {
+                            return(
+                                <tr key={user._id}>
+                                    <td>{index+1}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phone}</td>
+                                    <td>{user.role}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+
+        </>
     )
 }
