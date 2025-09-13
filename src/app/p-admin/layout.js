e23f7149f34df4656b6dbd7342cd6133daafb640
Milad Rastin <miladrastin@Milads-MacBook-Air.local>
2025-09-13 15:16:15 +0330

    feat(p-admin) : admin panel

---
+++ src/app/p-admin/layout.js
@@ -0,0 +1,32 @@
+import styles from "@/styles/p-admin/adminPanelLayout.module.css";
+import Sidebar from "@/components/modules/p-admin/Sidebar";
+import Topbar from "@/components/modules/p-admin/Topbor";
+//
+import { authUser } from "@/utils/auth";
+import { redirect } from "next/navigation";
+
+const AdminLayout = async ({ children }) => {
+  const user = await authUser();
+
+  if(!user) {
+      redirect('/login-register')
+  }
+  // only admin access
+  if( user.role !== "ADMIN" ) {
+    redirect('/p-user')
+  }
+
+  return (
+    <div className={styles.layout}>
+      <section className={styles.section}>
+        <Sidebar />
+        <div className={styles.contents}>
+          <Topbar />
+          {children}
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default AdminLayout;
---
+++ src/app/p-admin/products/page.js
@@ -0,0 +1,7 @@
+
+
+export default async function() {
+    return(
+        <h1>p-admin/products</h1>
+    )
+}
