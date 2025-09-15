915b368287ef27b18ae6337aeee64878e0c0bef4
Milad Rastin <miladrastin@Milads-MacBook-Air.local>
2025-09-15 16:59:24 +0330

    feat(utils) : auth-admin

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -55,8 +55,6 @@ export const validatePassword = (password) => {
 
 export const authUser = async () => {
 
-
-
     const cookieStore = await cookies()
   
     const token = cookieStore.get('token')
@@ -74,3 +72,27 @@ export const authUser = async () => {
 
     return user;
 }
+
+//protect api's using this helper 
+export const authAdmin = async () => {
+
+  await connectToDB();
+  const token = cookies().get("token");
+  let user = null;
+
+  if (token) {
+    const tokenPayload = verifyAccessToken(token.value);
+    if (tokenPayload) {
+      user = await UserModel.findOne({ email: tokenPayload.email });
+      if (user.role === "ADMIN") {
+        return user;
+      } else {
+        return null;
+      }
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+};
